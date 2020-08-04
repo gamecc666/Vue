8e20aef6d6ab8585bfccc7df77c695d008531358
gamecc666 <995611928@qq.com>
2020-08-04 17:57:10 +0800

    晚上思维导图

---
+++ firstproject/src/main.js
@@ -0,0 +1,9 @@
+/* jshint esversion: 6 */
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip=false;
+//Vue.config.devtools=false;
+new Vue({
+    render:h=>h(App)
+}).$mount('#app');
