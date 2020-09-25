1d5352cbb9e4202af61400db9be1ad37b2b331ea
gamecc666 <995611928@qq.com>
2020-09-25 14:42:13 +0800

    092501/vue.xmind & 测试项目更新

--- 新手项目练习/VSCode/practicepro/src/components/HelloWorld.vue
+++ 新手项目练习/VSCode/practicepro/src/components/HelloWorld.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="co">
     <p>{{message | captitalize}}</p>
+    <hr>
+    <div id="demo">
+      <button v-on:click="pSetClick">赋值</button>
+      <button v-on:click="pClearClick">清空</button>
+      <p ref="settext" id='setp'></p>
+      <p>v-model修饰符（.lazy/.number/trim）</p>
+      .lazy: <input type="text" v-model.lazy='lazyvalue' placeholder="请输入">
+      <p>输入框内容：{{lazyvalue}}</p>
+      .number: <input type="text" v-model.number='numbervalue' placeholder="请输入">
+      <p>输入框内容：{{numbervalue}},输入框类型：{{typeof numbervalue}}</p>
+      .trim: <input type="text" v-model.trim='trimvalue' placeholder="请输入">
+      <p>输入框内容：{{trimvalue}}</p>
+    </div>
   </div>
   <!-- <div class="hello">
     <h1>{{ msg }}</h1>
@@ -91,6 +104,9 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      trimvalue:'',
+      numbervalue:34,
+      lazyvalue:'',
       message: 'gamecc666 测试过滤器！！！'
     }
   },
@@ -100,12 +116,29 @@ export default {
       value=value.toString();
       return value.charAt(0).toUpperCase()+value.slice(1);
     }
+  },
+  methods:{
+    pSetClick:function(){
+      let pdom=this.$refs.settext;
+      pdom.innerHTML='gamecc666';
+    },
+    pClearClick:function(){
+      let pdom=this.$refs.settext;
+      pdom.innerHTML=null;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#setp{
+  background-color: bisque;
+  display: inline;
+}
+#demo,#co{
+  text-align: left;
+}
 h1, h2 {
   font-weight: normal;
 }
--- 新手项目练习/VSCode/practicepro/src/main.js
+++ 新手项目练习/VSCode/practicepro/src/main.js
@@ -1,8 +1,6 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router'         //这里引入router文件夹下面的index.js
 
 Vue.config.productionTip = false
 
